a2827ef5d89bab8b926b12754f9e1aae7a5b9caf
pablo-mansanet-bluefruit <pablo.mansanet@bluefruit.co.uk>
2021-03-17 17:07:31 +0100

    Formatting

--- src/drivers/efm32gg11b/flash.rs
+++ src/drivers/efm32gg11b/flash.rs
@@ -4,10 +4,13 @@ use bytemuck::cast_slice;
 use cortex_m_semihosting::hprintln;
 use efm32gg11b::{CMU, MSC};
 
-use crate::{hal::flash::ReadWrite, utilities::memory::Region, utilities::memory::IterableByOverlaps};
+use crate::{
+    hal::flash::ReadWrite,
+    utilities::memory::{IterableByOverlaps, Region},
+};
 
 pub struct Flash {
-    msc: MSC
+    msc: MSC,
 }
 
 #[derive(Copy, Clone, Debug)]
@@ -16,13 +19,12 @@ pub struct Map;
 #[derive(Copy, Clone, Debug)]
 pub struct Page(pub u16);
 
-
 impl Map {
-    pub fn pages() -> impl Iterator<Item=Page> { (0..count::PAGES as u16).map(Page) }
+    pub fn pages() -> impl Iterator<Item = Page> { (0..count::PAGES as u16).map(Page) }
 }
 
 impl Page {
-    pub fn address(&self) -> Address { Address(self.0 as u32 * size::PAGE as u32)}
+    pub fn address(&self) -> Address { Address(self.0 as u32 * size::PAGE as u32) }
 }
 
 #[derive(Copy, Clone, Debug)]
@@ -69,14 +71,16 @@ impl Into<usize> for Address {
 
 impl Flash {
     pub fn new(msc: MSC) -> Self {
-         const MSC_UNLOCK_CODE: u32 = 0x1B71;
-         // Safety: Unsafe access here is required only to write
-         // multiple bits at once to the same register. We must ensure
-         // that we write bits that leave the peripheral in a known and
-         // correct state.
-         unsafe { msc.lock.write(|w| w.bits(MSC_UNLOCK_CODE)); }
-         msc.writectrl.write(|w| w.wren().set_bit());
-         Self { msc }
+        const MSC_UNLOCK_CODE: u32 = 0x1B71;
+        // Safety: Unsafe access here is required only to write
+        // multiple bits at once to the same register. We must ensure
+        // that we write bits that leave the peripheral in a known and
+        // correct state.
+        unsafe {
+            msc.lock.write(|w| w.bits(MSC_UNLOCK_CODE));
+        }
+        msc.writectrl.write(|w| w.wren().set_bit());
+        Self { msc }
     }
 
     #[link_section = ".data"] // Must be executed from RAM
@@ -86,11 +90,15 @@ impl Flash {
     fn wait_until_not_busy(&self) { while self.is_busy() {} }
 
     #[link_section = ".data"] // Must be executed from RAM
-    fn wait_until_ready_to_write(&self) { while self.msc.status.read().wdataready().bit_is_clear() {} }
+    fn wait_until_ready_to_write(&self) {
+        while self.msc.status.read().wdataready().bit_is_clear() {}
+    }
 
     #[link_section = ".data"] // Must be executed from RAM
     fn erase_page(&mut self, page: Page) -> nb::Result<(), Error> {
-        if self.is_busy() { return Err(nb::Error::WouldBlock); }
+        if self.is_busy() {
+            return Err(nb::Error::WouldBlock);
+        }
         self.load_address(page.address())?;
         self.msc.writecmd.write(|w| w.erasepage().set_bit());
         self.wait_until_not_busy();
@@ -110,7 +118,13 @@ impl Flash {
 
     #[link_section = ".data"] // Must be executed from RAM
     fn verify_status(&self) -> nb::Result<(), Error> {
-        let error = self.msc.status.read().invaddr().bit_is_set().then_some(Error::InvalidAddress)
+        let error = self
+            .msc
+            .status
+            .read()
+            .invaddr()
+            .bit_is_set()
+            .then_some(Error::InvalidAddress)
             .or(self.msc.status.read().locked().bit_is_set().then_some(Error::MemoryIsLocked));
 
         if let Some(error) = error {
@@ -121,11 +135,7 @@ impl Flash {
     }
 
     #[link_section = ".data"] // Must be executed from RAM
-    fn write_page(
-        &mut self,
-        bytes: &[u8],
-        page: Page,
-    ) -> nb::Result<(), Error> {
+    fn write_page(&mut self, bytes: &[u8], page: Page) -> nb::Result<(), Error> {
         if bytes.len() != size::PAGE {
             return Err(nb::Error::Other(Error::MisalignedAccess));
         }
@@ -150,11 +160,13 @@ impl Flash {
 
 impl Drop for Flash {
     fn drop(&mut self) {
-         // Safety: Unsafe access here is required only to write
-         // multiple bits at once to the same register. We must ensure
-         // that we write bits that leave the peripheral in a known and
-         // correct state.
-        unsafe { self.msc.lock.write(|w| w.bits(0));}
+        // Safety: Unsafe access here is required only to write
+        // multiple bits at once to the same register. We must ensure
+        // that we write bits that leave the peripheral in a known and
+        // correct state.
+        unsafe {
+            self.msc.lock.write(|w| w.bits(0));
+        }
         self.msc.writectrl.write(|w| w.wren().clear_bit());
     }
 }
@@ -164,9 +176,7 @@ impl ReadWrite for Flash {
 
     type Address = Address;
 
-    fn label() -> &'static str {
-        "efm32gg11b flash (Internal)"
-    }
+    fn label() -> &'static str { "efm32gg11b flash (Internal)" }
 
     fn read(&mut self, address: Self::Address, bytes: &mut [u8]) -> nb::Result<(), Self::Error> {
         let inside_map = Map.contains(address) && Map.contains(address + bytes.len());
@@ -189,7 +199,7 @@ impl ReadWrite for Flash {
         let correctly_aligned_start = address_value & 0b11 == 0;
         let correctly_aligned_end = bytes.len() & 0b11 == 0;
         if !correctly_aligned_end || !correctly_aligned_start {
-            return Err(nb::Error::Other(Error::MisalignedAccess))
+            return Err(nb::Error::Other(Error::MisalignedAccess));
         }
         if self.is_busy() {
             return Err(nb::Error::WouldBlock);
@@ -217,7 +227,9 @@ impl ReadWrite for Flash {
 
     #[link_section = ".data"] // Must be executed from RAM
     fn erase(&mut self) -> nb::Result<(), Self::Error> {
-        if self.is_busy() { return Err(nb::Error::WouldBlock); }
+        if self.is_busy() {
+            return Err(nb::Error::WouldBlock);
+        }
         const MSC_MASS_ERASE_CODE: u32 = 0x0000631A;
         // Safety: Unsafe access here is required only to write
         // multiple bits at once to the same register. We must ensure
@@ -258,13 +270,10 @@ impl ReadWrite for Flash {
     }
 }
 
-
 mod size {
-    use super::count;
     pub const PAGE: usize = KB!(4);
 }
 
 mod count {
     pub const PAGES: usize = 512;
 }
-
