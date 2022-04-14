0d595b082509bbcdf60508eb91f1be174ba8d98d
Arron Speake <arron.speake@bluefruit.co.uk>
2022-04-14 12:27:43 +0100

    Refactor pin trait usage.

--- src/drivers/max3263/spi.rs
+++ src/drivers/max3263/spi.rs
@@ -1,33 +1,37 @@
+//! SPI driver implementation for the MAX32631.
+//!
+//! Currently, only 4-wire SPI over the SPIM1 peripheral is supported.
+
 use core::marker::PhantomData;
 use crate::hal::spi;
+use crate::drivers::max3263::gpio::*;
 
+/// A pin which may be used as an SPI clock pin.
 pub unsafe trait ClkPin {}
+
+/// A pin which may be used as an SPI chip select pin.
 pub unsafe trait CsPin {}
+
+/// A pin which may be used as an SPI IO0 (MOSI) pin.
 pub unsafe trait Io0Pin {}
+
+/// A pin which may be used as an SPI IO1 (MISO) pin.
 pub unsafe trait Io1Pin {}
 
-pub struct Spi<PINS> {
+// Trait implementations for SPI pins.
+unsafe impl ClkPin for P10<AF0> {}
+unsafe impl CsPin for P13<AF0> {}
+unsafe impl Io0Pin for P11<AF0> {}
+unsafe impl Io1Pin for P12<AF0> {}
+
+pub struct Spi<CK: ClkPin, CS: CsPin, IO0: Io0Pin, IO1: Io1Pin> {
     // TODO: Require PINS type parameter for later use.
-    _phantom: PhantomData<PINS>,
+    _phantom: PhantomData<(CK, CS, IO0, IO1)>,
 }
 
 pub enum Error {}
 
-#[macro_export(local_inner_macros)]
-macro_rules! enable_qspi {
-    () => {
-        #[cfg(feature = "max32631")]
-        seal_pins!(blue_hal::drivers::max3263::spi::ClkPin: [P10]);
-        #[cfg(feature = "max32631")]
-        seal_pins!(blue_hal::drivers::max3263::spi::CsPin: [P13]);
-        #[cfg(feature = "max32631")]
-        seal_pins!(blue_hal::drivers::max3263::spi::Io0Pin: [P11]);
-        #[cfg(feature = "max32631")]
-        seal_pins!(blue_hal::drivers::max3263::spi::Io1Pin: [P12]);
-    }
-}
-
-impl<PINS> Spi<PINS> {
+impl<CK: ClkPin, CS: CsPin, IO0: Io0Pin, IO1: Io1Pin> Spi<CK, CS, IO0, IO1> {
     pub fn new() -> Self {
         Self {
             _phantom: Default::default(),
@@ -35,7 +39,7 @@ impl<PINS> Spi<PINS> {
     }
 }
 
-impl<PINS> spi::FullDuplex<u8> for Spi<PINS> {
+impl<CK: ClkPin, CS: CsPin, IO0: Io0Pin, IO1: Io1Pin> spi::FullDuplex<u8> for Spi<CK, CS, IO0, IO1> {
     type Error = Error;
 
     fn transmit(&mut self, _word: Option<u8>) -> nb::Result<(), Self::Error> {
