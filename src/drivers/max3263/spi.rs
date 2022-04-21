b36908fafc0e9444d36b1f75bee780f060757178
Arron Speake <arron.speake@bluefruit.co.uk>
2022-04-21 10:27:19 +0100

    Add pin fields to Spi struct.

--- src/drivers/max3263/spi.rs
+++ src/drivers/max3263/spi.rs
@@ -3,19 +3,22 @@
 //! Currently, only 4-wire SPI over the SPIM1 peripheral is supported.
 
 use core::marker::PhantomData;
-use crate::hal::spi;
-use crate::drivers::max3263::gpio::*;
+use crate::{
+    hal::spi,
+    drivers::max3263::gpio::*,
+    max32pac::{SPIM1, CLKMAN},
+};
 
-/// A pin which may be used as an SPI clock pin.
+/// A pin which may be used as an SPIM1 clock pin.
 pub unsafe trait ClkPin {}
 
-/// A pin which may be used as an SPI chip select pin.
+/// A pin which may be used as an SPIM1 chip select pin.
 pub unsafe trait CsPin {}
 
-/// A pin which may be used as an SPI IO0 (MOSI) pin.
+/// A pin which may be used as an SPIM1 IO0 (MOSI) pin.
 pub unsafe trait Io0Pin {}
 
-/// A pin which may be used as an SPI IO1 (MISO) pin.
+/// A pin which may be used as an SPIM1 IO1 (MISO) pin.
 pub unsafe trait Io1Pin {}
 
 // Trait implementations for SPI pins.
@@ -24,18 +27,20 @@ unsafe impl CsPin for P13<AF0> {}
 unsafe impl Io0Pin for P11<AF0> {}
 unsafe impl Io1Pin for P12<AF0> {}
 
+/// The SPI driver.
 pub struct Spi<CK: ClkPin, CS: CsPin, IO0: Io0Pin, IO1: Io1Pin> {
-    // TODO: Require PINS type parameter for later use.
-    _phantom: PhantomData<(CK, CS, IO0, IO1)>,
+    spim: SPIM1,
+    clock: CK,
+    chip_select: CS,
+    mosi: IO0,
+    miso: IO1,
 }
 
 pub enum Error {}
 
 impl<CK: ClkPin, CS: CsPin, IO0: Io0Pin, IO1: Io1Pin> Spi<CK, CS, IO0, IO1> {
-    pub fn new() -> Self {
-        Self {
-            _phantom: Default::default(),
-        }
+    pub fn new(spim: SPIM1, _clkman: &mut CLKMAN, clock: CK, chip_select: CS, mosi: IO0, miso: IO1) -> Self {
+        Self { spim, clock, chip_select, mosi, miso }
     }
 }
 
